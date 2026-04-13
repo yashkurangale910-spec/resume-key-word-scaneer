26659093bede98a71045ebcd78fc3a71dbb1699d
Your Name <you@example.com>
2026-04-13 12:13:13 +0530

    chore: add .gitignore and sync all project files

---
+++ src/main/java/com/resumescanner/ResumeKeywordScannerApplication.java
@@ -0,0 +1,11 @@
+package com.resumescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeKeywordScannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeKeywordScannerApplication.class, args);
+    }
+}
